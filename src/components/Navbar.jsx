1d201bc4e9c284457859f95976db631fe308e128
nitootin <phillip.profissional@gmail.com>
2025-06-16 21:08:26 -0300

    Feito a Navbar e alterado o app.js (não funcionando)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
 
     return(
         <nav>
             <ul>
-                
+                <li><Link to={"/"}> Chamados</Link></li>
+                {user.role === "ADMINISTRADOR" && <li><Link to={"/Usuarios"}> Usuarios </Link></li>}
             </ul>
+            <div className="user-info">
+                {user ? (
+                    <span>Bem vindo</span>
+                ):(
+                    <span>você não está logado</span>
+                )}
+            </div>
 
 
 
